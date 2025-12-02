c2caef074ebcc0d98d309fb3d23412204385bf4d
melizahaiziK <melizaha1zik@gmail.com>
2025-12-02 22:15:09 +0700

    modifikasi percobaan 2 pertanyaan no.6

--- Percobaan2.java
+++ Percobaan2.java
@@ -1,12 +1,19 @@
 public class Percobaan2 {
    public static void main(String[] var0) {
-      Menu("Andi" , true);
+      Menu("Andi" ,true, "DISKON50");
    }
-   public static void Menu(String namaPelanggan, boolean isMember) {
+   public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
     System.out.println("Selamat datang, " + namaPelanggan + "!");
 
       if (isMember){
         System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+      }
+      if (kodePromo.equals("DISKON50")) {
+         System.out.println("Kode promo valid! Anda mendapatkan diskon 50%.");
+      } else if (kodePromo.equals("DISKON30")) {
+         System.out.println("Kode promo valid! Anda mendapatkan diskon 30%.");
+      } else {
+         System.out.println("Kode promo invalid.");
       }
       System.out.println("===== MENU RESTO KAFE =====");
       System.out.println("1. Kopi Hitam Rp 15,000");
