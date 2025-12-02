5569397a5d9eb04ba83f6e9b003d116fdb57050b
melizahaiziK <melizaha1zik@gmail.com>
2025-12-02 21:34:00 +0700

    Percobaan 2

---
+++ Percobaan2.java
@@ -0,0 +1,22 @@
+public class Percobaan2 {
+   public static void main(String[] var0) {
+      Menu("Andi" , true);
+   }
+   public static void Menu(String namaPelanggan, boolean isMember) {
+    System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+      if (isMember){
+        System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+      }
+      System.out.println("===== MENU RESTO KAFE =====");
+      System.out.println("1. Kopi Hitam Rp 15,000");
+      System.out.println("2. Cappuccino Rp 20,000");
+      System.out.println("3. Latte Rp 22,000");
+      System.out.println("4. Teh Tarik Rp 12,000");
+      System.out.println("5. Roti Bakar Rp 10,000");
+      System.out.println("6. Mie Goreng Rp 18,000");
+      System.out.println("===========================");
+      System.out.println("Silakan pilih menu yang Anda inginkan.");
+   }
+}
+    
